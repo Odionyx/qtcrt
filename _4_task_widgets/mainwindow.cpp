cedfe2a47f9b8d4a6b7666072be1953397ee65c0
Maks_Nik_CPP7 <odionyx@yandex.ru>
2024-02-26 13:21:33 +0400

    Change _4_task

--- _4_task_widgets/mainwindow.cpp
+++ _4_task_widgets/mainwindow.cpp
@@ -7,8 +7,25 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    /* Progress bard */
+    /* Name */
+    ui->radioButton->setText("Вкл.");
+    ui->radioButton_2 ->setText("Выкл.");
+    ui->pushButton->setText("Выполнить");
+    ui->groupBox->setTitle("Соединение");
+    ui->groupBox_2->setTitle("Таблица");
 
+    /* drop list */
+    ui->comboBox->addItem("Создать");
+    ui->comboBox->addItem("Добавить столбец");
+    ui->comboBox->addItem("Добавить строку");
+    ui->comboBox->addItem("Удалить строку");
+    ui->comboBox->addItem("Удалить столбец");
+    ui->comboBox->addItem("Удалить");
+
+    /*  */
+    ui->pushButton->setChecked(false);
+
+    /* Progress bard */
     ui->progressBar->setMaximum(10);
     ui->progressBar->setMinimum(0);
     ui->progressBar->setValue(0);
@@ -19,14 +36,11 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_pushButton_toggled(bool checked)
 {
-    int count = ui->spinBox->value();
-    if(count>9){
-        count=-1;
-    }
-    ui->spinBox->setValue(++count);
+    if((ui->spinBox->value())< 10){
+        ui->spinBox->setValue((ui->spinBox->value()+1));
+    } else {ui->spinBox->setValue(0);}
 }
 
 void MainWindow::on_spinBox_valueChanged(int arg1)
